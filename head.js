7c58e556a347b90bb8ead0799ebaf9764b109c9b
Sethu Nair <sethunair2102@gmail.com>
2023-03-25 17:01:28 -0600

    head function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+
+const head = function(array) {
+  return array[0]
+};
+
+console.log(head([5,6,7]));
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
