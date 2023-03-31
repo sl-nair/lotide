c0156fa835a08cbcef9c0fcad80192dcd888532f
Sethu Nair <sethunair2102@gmail.com>
2023-03-31 15:29:32 -0600

    completed findKeyByValue assignment

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+
+const findKeyByValue  = function(theObject,value) {
+  //scan the object to see what index the key of the value is at
+  for (let eachKey in theObject) {
+    //need to check if the value is equal to any value in the object
+    if (value === theObject[eachKey]) {
+      return eachKey;
+    }
+  }
+};
+
+
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎👎👎 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), 'drama');
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
