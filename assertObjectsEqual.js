91b3c2e8382965755cb0d295c41d23a21d46f4c8
Sethu Nair <sethunair2102@gmail.com>
2023-04-01 18:05:06 -0600

    assert equal for objects

---
+++ assertObjectsEqual.js
@@ -0,0 +1,52 @@
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
+  if (Object.keys(actual).length !== Object.keys(expected).length) {
+    console.log(`👎👎👎 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+    return;
+  }
+
+  for (let keys1 in actual) {
+    if (Object.prototype.hasOwnProperty.call(expected, keys1)) {
+      if (Array.isArray(actual[keys1])) {
+        if (!eqArrays(actual[keys1], expected[keys1])) {
+          console.log(`👎👎👎 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+          return;
+        }
+      } else if (actual[keys1] !== expected[keys1]) {
+        console.log(`👎👎👎 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+        return;
+      }
+    } else {
+      console.log(`👎👎👎 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+      return;
+    }
+  }
+  console.log(`👍👍👍 Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  return;
+};
+
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertObjectsEqual(shirtObject , anotherShirtObject); // => true
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertObjectsEqual(shirtObject , longSleeveShirtObject); // => false
+
+
+
+const multiColorShirtObject = { colors: ["red", "blue"]};
+const anotherMultiColorShirtObject = {colors: ["red", "blue"] };
+assertObjectsEqual(multiColorShirtObject, anotherMultiColorShirtObject);
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+assertObjectsEqual(multiColorShirtObject  , longSleeveMultiColorShirtObject);
