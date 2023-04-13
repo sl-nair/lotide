68572538b97f3ef68663d4357e43df8af06dc353
Sethu Nair <sethunair2102@gmail.com>
2023-04-12 23:32:11 -0600

    refactored with modules

--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual,expected) {
-  if (actual === expected) {
-    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎👎👎 Assertion Failed: ${actual} === ${expected}`);
-  }
-};
-
-
 const head = function(array) {
-  return array[0]
+  return array[0];
 };
 
-console.log(head([5,6,7]));
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
