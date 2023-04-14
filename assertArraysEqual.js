b08af8315c8b3f190c660cda8fee56140f2a43d6
Sethu Nair <sethunair2102@gmail.com>
2023-04-13 19:18:39 -0600

    refactored

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertArraysEqual = function(array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`👎👎👎 Assertion Failed: ${array1} !== ${array2}`);
+      return;
+    }
+  }
+  console.log(`👍👍👍 Assertion Passed: ${array1} === ${array2}`);
+  return;
+};
+
+module.exports = assertArraysEqual
