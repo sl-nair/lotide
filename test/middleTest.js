4249afd260a22764ba614a718991d7483478e84d
Sethu Nair <sethunair2102@gmail.com>
2023-04-13 19:49:36 -0600

    mocha and chai used for test

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,9 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
+describe("#middleTest", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+});
 
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4])
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
