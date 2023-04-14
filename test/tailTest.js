72e0161548c8a90fea5e5bf502f80e77b5ecdf21
Sethu Nair <sethunair2102@gmail.com>
2023-04-13 19:46:40 -0600

    used mocha and chai for testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,22 @@
 const tail = require('../tail')
 const assertEqual = require('../assertEqual')
 
-console.log(tail([5,6,7,8,9]));
-console.log(tail([5]));
-console.log(tail([]));
+const assert = require('chai').assert;
+
+describe("tailTest", () => {
+  it("returns [7] for [6,7]", () => {
+    assert.deepEqual(tail([6,7]), [7]);
+  });
+  it("returns [\"Lighthouse\"] for [\"Hello\", \"Lighthouse\"]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse"]), ["Lighthouse"]);
+  });
+})
 
 
-assertEqual(tail([6,7]), 7);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// console.log(tail([5,6,7,8,9]));
+// console.log(tail([5]));
+// console.log(tail([]));
+
+
+// assertEqual(tail([6,7]), 7);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
