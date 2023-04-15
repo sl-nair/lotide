c125b285599ebe4800741d67d3adddb7910e4e51
Sethu Nair <sethunair2102@gmail.com>
2023-04-15 10:31:32 -0600

    all functions are in an object

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const assertArraysEqual = require("./assertArraysEqual");
 const assertEqual = require('./assertEqual');
 const assertObjectsEqual = require('./assertObjectsEqual');
 const countLetters = require('./countLetters');
+const countOnly = require("./countOnly");
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObject');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map')
+const takeUntil = require('./takeUntil')
+const without = require('./without')
 
 
 module.exports = {
@@ -16,5 +26,14 @@ module.exports = {
   head: head,
   tail: tail,
   middle: middle,
-
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey:findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions:letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
 };
